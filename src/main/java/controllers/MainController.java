c2bbbce733ffec3f779c1d872f9dfa5a8c82817c
antoniopl6 <72573790+antoniopl6@users.noreply.github.com>
2023-06-06 12:34:46 +0200

    Revert "Personalized timeline which shows tweets only from the users they follow"
    This reverts commit 59b728a7024352e91c403388dec78cea221ac0ab.

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -3,7 +3,6 @@ package controllers;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -60,27 +59,14 @@ public class MainController extends HttpServlet {
 			userManager.finalize();
 			request.setAttribute("user", user);
 			// Load templates
-			if(Objects.nonNull(user.getName())) {
-				request.setAttribute("menu","ViewMenuLogged.jsp");
-			}
-			else {
-				request.setAttribute("menu","ViewMenuNotLogged.jsp");
-			}
+			request.setAttribute("menu","ViewMenuNotLogged.jsp");
 			request.setAttribute("content","ViewUserInfo.jsp");
 		}
 		// Registered and go home
 		else {
-			// Load all tweets
-			List<Tweet> tweets = Collections.emptyList();
-			ManageTweets tweetManager = new ManageTweets();
-			User user = (User) session.getAttribute("user");
-			tweets = tweetManager.getLoggedTweets(user.getId(), 0, 10);
-			tweetManager.finalize();
-			request.setAttribute("tweets",tweets);
-			request.setAttribute("user", user);
 			System.out.println("Main Controller: active session has been found.");
 			request.setAttribute("menu","ViewMenuLogged.jsp");
-			request.setAttribute("content","ViewTweets.jsp");
+			request.setAttribute("content","ViewOwnTimeline.jsp");
 		}
 		
 		RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
